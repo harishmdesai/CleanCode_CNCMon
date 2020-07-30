0064276c3628fd6359217b8b72fd88852bcac658
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-07-30 21:35:07 +0530

    Data code definitions

---
+++ CNNMon_DataCode.h
@@ -0,0 +1,11 @@
+#ifndef CNNMON_DATACODE_H__
+#defien CNNMON_DATACODE_H__
+
+// Defining Default values
+#define SelfTestStatusCode_AllOk 0xFF
+#define SelfTestStatusCode_NoData 0x00
+#define SelfTestStatusCode_ControllerBoardNotOk 0x01
+#define SelfTestStatusCode_ConfigDataCorrupted 0x02 
+
+
+#endif
