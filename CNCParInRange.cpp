167a3696f24edab3035281f298c8bc3e0816f4fa
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-08 13:52:14 +0530

    Create CNCParInRange.cpp

---
+++ CNCParInRange.cpp
@@ -0,0 +1,15 @@
+#include "CNCParInRange.hpp"
+
+bool IsParmInRange(float arg1,float arg2)
+{
+   bool l_return = FALSE;
+   if(arg1 < arg2)
+   {
+      l_return = TRUE;
+   }
+   
+   return l_return;
+}
+
+
+#endif
