8cdb2fbfcb667cdbc37c66e199ec7aaff95b2703
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-08 13:45:49 +0530

    Create CNCMain.hpp

---
+++ CNCMain.hpp
@@ -0,0 +1,14 @@
+#ifndef CNCMAIN_HPP__
+#define CNCMAIN_HPP__
+
+typedef enum 
+{
+    CNCOperatingTemp,
+    CNCPartDimenVariation,
+    CNCOnDutration,
+    CNCSelfTestStatus,
+    CNCMonListended
+}CNC_MonList;
+
+
+#endif
