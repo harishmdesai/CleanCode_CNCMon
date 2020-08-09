f11a6e73cde1017264792bd2e8be93ae50a7311c
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-09 19:34:54 +0530

    Create CNCDiamValidator.hpp

---
+++ CNCDiamValidator.hpp
@@ -0,0 +1,10 @@
+#ifndef "CNCDIMAVALIDATION_HPP__"
+#define "CNCDIMAVALIDATION_HPP__"
+
+extern bool Fun_GetCurrentDiam(void);
+
+extern bool CNCDiamValidator(void);
+
+extern void fun_SetCurrentDiam(float l_CNCCurrentDiam);
+
+#endif
