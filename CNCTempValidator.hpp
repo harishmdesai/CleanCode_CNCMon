e508ab582c7750b757269cc405f9eab5e7a44d6a
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-09 19:39:09 +0530

    Create CNCTempValidator.hpp

---
+++ CNCTempValidator.hpp
@@ -0,0 +1,9 @@
+#ifndef CNCTEMPVALIDATION_HPP__
+#define CNCTEMPVALIDATION_HPP__
+
+extern void fun_SetCurrentTemp(float l_CNCCurrentValue);
+extern bool CNCTempOutOfRange(void);
+
+extern bool Fun_GetCurrentTemp(void);
+
+#endif
