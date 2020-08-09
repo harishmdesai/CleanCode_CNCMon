b96f6f363f007903f6590b9afc9b02e6be1ff9a9
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-09 19:36:16 +0530

    Create CNCDurationValidation.hpp

---
+++ CNCDurationValidation.hpp
@@ -0,0 +1,11 @@
+#ifndef CNCDURATIONVALIDATION_H__
+#define CNCDURATIONVALIDATION_H__
+
+
+extern bool Fun_GetCurrentDuration(void);
+
+extern bool CNCDurationexceeded(void);
+
+extern void fun_SetSystemDuration(int l_CNCSystemDuration);
+
+#endif
