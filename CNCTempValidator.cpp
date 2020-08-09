131a1006aff2664c2406c6345b4add3346752488
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-09 19:38:32 +0530

    Create CNCTempValidator.cpp

---
+++ CNCTempValidator.cpp
@@ -0,0 +1,20 @@
+#include "CNCTempValidator.hpp"
+
+static bool TempOutofRange;
+#define AllowedTempRange 35 /*In Degree C*/
+
+void fun_SetCurrentTemp(float l_CNCCurrentValue)
+{
+	g_CNCCurrentValue.tempearature = l_CNCCurrentValue;
+}
+
+bool Fun_GetCurrentTemp(void)
+{
+	float l_Temp = g_CNCCurrentValue.tempearature;
+	TempOutofRange = !if(IsParmWithinARange(l_Temp, AllowedTempRange));
+}
+
+bool CNCTempOutOfRange(void)
+{
+	return TempOutofRange;
+}
