7b836272d308a550c75873ac8c3f5f69b6829369
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-10 09:21:39 +0530

    Update CNCDiamValidator.cpp

--- CNCDiamValidator.cpp
+++ CNCDiamValidator.cpp
@@ -3,7 +3,7 @@
 static bool DimensionOutOfRange ;
 #define AllowedDiamRange 0.05
 
-void Fun_GetCurrentDiam()
+void Fun_GetCurrentDiam(void)
 {
 	float l_Diam = g_CNCCurrentValue.Diameter;
 	DimensionOutOfRange = !if(IsParmWithinARange(l_Diam, AllowedDiamRange));
